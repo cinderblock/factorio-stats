292649db61aa2a32ae881659d057ce0be6fbddbb
Cameron Tacklind <cameron@tacklind.com>
2024-11-29 15:41:06 -0800

    No, this was better

--- ui/src/FactoryStatus.tsx
+++ ui/src/FactoryStatus.tsx
@@ -84,34 +84,7 @@ export default function FactoryStatus() {
 
   return (
     <div>
-      <h2>Factory Status</h2>
       <div>
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-          <table>
-            <tbody>
-              <tr>
-                <td align="left">
-                  <strong>Factory Age</strong>
-                </td>
-                <td align="left">
-                  {typeof stats.time === 'number' ? <DurationDisplay hours={stats.time} /> : stats.time}
-                </td>
-              </tr>
-              <tr>
-                <td align="left">
-                  <strong>Version</strong>
-                </td>
-                <td align="left">{stats.version}</td>
-              </tr>
-              <tr>
-                <td align="left">
-                  <strong>Seed</strong>
-                </td>
-                <td align="left">{stats.seed}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
         <div style={{ display: 'flex', justifyContent: 'center' }}>
           <table>
             <thead>
@@ -191,6 +164,32 @@ export default function FactoryStatus() {
                   <td align="right">{toPercent(stats.evolution[planet].kills)}</td>
                 </tr>
               ))}
+            </tbody>
+          </table>
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <table>
+            <tbody>
+              <tr>
+                <td align="left">
+                  <strong>Factory Age</strong>
+                </td>
+                <td align="left">
+                  {typeof stats.time === 'number' ? <DurationDisplay hours={stats.time} /> : stats.time}
+                </td>
+              </tr>
+              <tr>
+                <td align="left">
+                  <strong>Version</strong>
+                </td>
+                <td align="left">{stats.version}</td>
+              </tr>
+              <tr>
+                <td align="left">
+                  <strong>Seed</strong>
+                </td>
+                <td align="left">{stats.seed}</td>
+              </tr>
             </tbody>
           </table>
         </div>
